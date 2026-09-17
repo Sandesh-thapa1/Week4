0214d20e535edbe39c2a1ae1abf246bf64ac4577
Sandesh-thapa1 <sandest@metropolia.fi>
2026-09-18 00:31:01 +0300

    Complete hooks lab 1

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,41 +1,23 @@
+import {useEffect, useState} from 'react';
 import MediaRow from '../components/MediaRow';
+import {fetchData} from '../utils/fetchData';
 
 const Home = () => {
-  const mediaArray = [
-    {
-      media_id: 8,
-      user_id: 5,
-      filename: 'https://picsum.photos/seed/picture1/1200/800',
-      thumbnail: 'https://picsum.photos/seed/thumb1/320/240',
-      filesize: 170469,
-      media_type: 'image/jpeg',
-      title: 'Picture 1',
-      description: 'This is a placeholder picture.',
-      created_at: '2024-01-07T20:49:34.000Z',
-    },
-    {
-      media_id: 9,
-      user_id: 7,
-      filename: 'https://picsum.photos/seed/picture2/1200/800',
-      thumbnail: 'https://picsum.photos/seed/thumb2/320/240',
-      filesize: 1002912,
-      media_type: 'image/jpeg',
-      title: 'Pic 2',
-      description: 'This is another picture.',
-      created_at: '2024-01-07T21:32:27.000Z',
-    },
-    {
-      media_id: 17,
-      user_id: 2,
-      filename: 'https://www.w3schools.com/html/mov_bbb.mp4',
-      thumbnail: 'https://picsum.photos/seed/thumb3/320/240',
-      filesize: 1236616,
-      media_type: 'video/mp4',
-      title: 'Bunny',
-      description: 'Butterflies fly around the bunny.',
-      created_at: '2024-01-07T20:48:13.000Z',
-    },
-  ];
+  const [mediaArray, setMediaArray] = useState([]);
+
+  useEffect(() => {
+    const getMedia = async () => {
+      try {
+        const json = await fetchData('test.json');
+        setMediaArray(json);
+        console.log(json);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getMedia();
+  }, []);
 
   return (
     <>
